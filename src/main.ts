40198e71e704b78b316126a9b6ada9824be34e0d
manhhiep <doanmanhhiep.2003@gmail.com>
2026-07-08 22:36:20 +0700

    feat: update CORS configuration to use dynamic API URL from config service

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from 'src/config/swagger.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.enableShutdownHooks();
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: configService.get<string>('VITE_API_URL'),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
