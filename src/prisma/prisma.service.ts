f58cea8f8b06ddc1a2292460822ed4877adb678a
manhhiep <doanmanhhiep.2003@gmail.com>
2026-07-08 21:20:56 +0700

    feat: add SSL support for database connections using CA certificate

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,16 +3,23 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor(configService: ConfigService) {
     const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+
     const pool = new Pool({
       connectionString,
+      ssl: {
+        ca: readFileSync(join(process.cwd(), 'certs', 'ca.pem'), 'utf8'),
+      },
     });
 
     const adapter = new PrismaPg(pool);
+
     super({ adapter });
   }
 
